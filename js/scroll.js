e2b67ac025809fd750086a8d55a158ef8935dad2
Brian Han <bmhan319@hotmail.com>
2018-09-07 12:24:01 -0700

    initial commit, finished main page, not repsonsive yet

---
+++ js/scroll.js
@@ -0,0 +1,67 @@
+//Added JQuery for smooth scroll when selecting nav item
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1500, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+
+
+
+// Add active highlight to the current button
+var header = document.getElementById("navbar");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+
+
+
+//Top Button appears on scroll down
+document.getElementById('top').onscroll = function() {
+  if ($(document).scrollTop() >= 300) {
+    $('#top-button').stop().animate({opacity:1}, 200, 'linear');
+  
+  //Top Button disappears on scroll up
+  } else if ($(document).scrollTop() < 300) {
+    $('#top-button').stop().animate({opacity:0}, 200, 'linear');
+  } 
+  
+  //Top Button moves position when scrollbar gets to the bottom
+  var element = document.getElementById("top-button");
+  if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
+    element.classList.remove("top-button");
+    element.classList.add("top-button-moved");
+  } else if ((window.innerHeight + window.scrollY) < document.body.scrollHeight) {
+    element.classList.remove("top-button-moved");
+    element.classList.add("top-button");
+  }
+};
+
+
+
+
